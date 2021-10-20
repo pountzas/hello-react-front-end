cb1c01059660af0ff61b5ce5ee2602e1e085e499
pountzas <nikos@pountzas.gr>
2021-10-21 02:18:06 +0300

    create component Greeting

---
+++ src/components/Greeting.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const GET_GREETINGS_REQUEST = 'GET_GREETINGS_REQUEST';
+const GET_GREETINGS_SUCCESS = 'GET_GREETINGS_SUCCESS';
+
+function getGreetings() {
+  return (dispatch) => {
+    dispatch({ type: GET_GREETINGS_REQUEST });
+    return fetch('http://localhost:3001/v1/greetings')
+      .then((response) => response.json())
+      .then((json) => dispatch(getGreetingsSuccess(json)))
+      .catch((error) => error);
+  };
+};
+
+function getGreetingsSuccess(json) {
+  return {
+    type: GET_GREETINGS_SUCCESS,
+    json
+  };
+};
+
+class Greeting extends React.Component {
+  componentDidMount() {
+    this.props.getGreetings()
+  }
+  render () {
+    const { greetings } = this.props;
+    return (
+      <React.Fragment>
+        <h1>{ greetings[0].message }</h1>
+      </React.Fragment>
+    );
+  }
+}
+const structuredSelector = createStructuredSelector({
+  greetings: state => state.greetings
+});
+
+const mapDispatchToProps = { getGreetings };
+
+export default connect(structuredSelector, mapDispatchToProps)(Greeting);
