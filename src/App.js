7ed80eca19bcfb90fdd5bb4666932fc26b2b53f7
pountzas <nikos@pountzas.gr>
2021-10-21 02:18:35 +0300

    modify app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom"
+import { Provider } from "react-redux"
+import configureStore from "./configureStore"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Greeting from "./components/Greeting"
+
+const store = configureStore();
+
+class App extends React.Component {
+  render () {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => <Link to="/greeting">Get greeting from the API</Link>} />
+            <Route path="/greeting" render={() => <Greeting />} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
-export default App;
+export default App
